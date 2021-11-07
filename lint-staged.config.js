a5aabec8a8161228730618d6b9d736fd8bcaf44c
Nguyễn Việt Thái <vietthai2512@gmail.com>
2021-11-07 21:00:17 +0700

    chore: setup lint, prettier, env

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  '**/*.{js,ts,tsx}': (filenames) =>
+    `next lint --fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
+  '*': 'prettier --ignore-unknown --write'
+};
